0915b1d8953d53704a8d6150fbf3befa4e2554de
Artem Semkin <art.krsk@gmail.com>
2025-09-13 19:05:09 +0800

    wip

--- src/frontend/index.ts
+++ src/frontend/index.ts
@@ -8,3 +8,7 @@ export * from "./main";
 export * from "./managers";
 export * from "./utils";
 export * from "./handlers";
+export * from "./document-management";
+export * from "./enhanced-handlers-manager";
+export * from "./enhanced-utilities";
+export * from "./integrated-interface";
