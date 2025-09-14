6043a359b5fc05f1e4f499e701d003e4e54542cf
Artem Semkin <art.krsk@gmail.com>
2025-09-14 18:12:10 +0800

    wip

--- src/globals/index.ts
+++ src/globals/index.ts
@@ -6,5 +6,6 @@
 export * from "./window";
 export * from "./elementor-modules";
 export * from "./elementor-window";
+export * from "./elementor-global";
 export * from "./frontend-handler-interfaces";
 export * from "./hooks";
