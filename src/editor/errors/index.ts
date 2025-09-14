06998cf85bdff508a77cdecd9b124e4e19eed344
Artem Semkin <art.krsk@gmail.com>
2025-09-15 07:45:01 +0800

    wip

---
+++ src/editor/errors/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Editor Errors System
+ *
+ * Export all editor error types and classes
+ */
+
+export * from './element-type-not-found';
+
+export { default as ElementTypeNotFound } from './element-type-not-found';
---
+++ src/editor/introduction-tooltips/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Introduction Tooltips System
+ *
+ * Export all introduction tooltips interfaces and types
+ */
+
+export * from './manager';
+
+export { default as IntroductionTooltipsManager } from './manager';
+export type { IntroductionTooltip, TooltipDialogElements } from './manager';
