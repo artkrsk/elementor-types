51333c973c00e9b82e5b0aa55d614b8b3fe7d7b8
Artem Semkin <art.krsk@gmail.com>
2025-09-15 09:31:27 +0800

    wip

--- simple-test.ts
+++
@@ -1,8 +0,0 @@
-// Simple test to check individual element import
-import type { ElementModel } from './src/editor/elements/models/element';
-import type { BaseElementModel } from './src/editor/elements/models/base-element-model';
-
-const test: ElementModel = {} as any;
-const test2: BaseElementModel = {} as any;
-
-console.log('Simple test passed');
